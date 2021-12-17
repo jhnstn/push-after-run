cd294acf3a70ba03185d5655528f88e8d4037b53
jhnstn <jason@readysetandco.com>
2021-12-17 09:13:12 -0500

    Drop post run event and adding a command run

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// No op
-() => {};
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const simpleGit = require("simple-git");
+
+const git = simpleGit();
+
+try {
+  await exec.exec(core.getInput("run_command"));
+  const status = git.status();
+  console.log(status);
+} catch (error) {
+  core.setFailed(error.message);
+}
